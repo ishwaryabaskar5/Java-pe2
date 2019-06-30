51fdc940adc79a9b342ec4515d7d62c3101a18d9
isbaska <isbaska1@in.ibm.com>
2019-06-30 19:50:42 +0530

    FindFile.java issue fixes

--- src/main/java/com/stackroute/pe2/FindFile.java
+++ src/main/java/com/stackroute/pe2/FindFile.java
@@ -10,13 +10,32 @@ public class FindFile{
 	public void findFileByExtension(){
 
 //		declaration and initialization
-		File folder = new File("/home/ishwarya/IdeaProjects/Java-PE2/src/main/java/com/stackroute/pe2");
+		String path = "/home/ishwarya/IdeaProjects/Java-PE2/src/main/java/com/stackroute/pe2";
+		File folder = new File(path);
 		File[] files = folder.listFiles();
+		String targetFile = "", str = "";
+		BufferedReader br;
 		
-		for (File a: files){
-			tar = a.getName().toLowerCase().endsWith(".doc");
-		}
+		try {
+			for (File a: files) {
+				if (a.getName().endsWith(".doc") == true) {
+					targetFile = a.getName();
+					br = new BufferedReader(new FileReader(path+"/"+targetFile));
+//	                read a file lines until it is null
+					while ((str=br.readLine())!= null)
+					{
+						System.out.print(str); 					}
+					br.close();
+					
+					
+				}
+			}
+			} catch (IOException e){
+				System.out.print("IO Exception");
+			}
+		
 	
 	}
 
 }
+
